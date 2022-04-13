52bb383f0789380f2be95c8633d484a153015157
Murillo R. Araújo <mr_araujo@icloud.com>
2022-04-13 10:05:53 -0300

    Initial commit

---
+++ Netflix-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-Clone
+//
+//  Created by Murillo R. Araújo on 12/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
